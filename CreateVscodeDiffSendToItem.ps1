5737d293f5bad830caa572e97af2302d805c3d27
Michał <rogalinsky@gmail.com>
2020-09-17 17:58:09 +0200

    Update CreateVscodeDiffSendToItem.ps1

--- CreateVscodeDiffSendToItem.ps1
+++ CreateVscodeDiffSendToItem.ps1
@@ -1,8 +1,23 @@
-$ExePath = [System.IO.Path]::GetFullPath("$env:AppData\..\Local\Programs\Microsoft VS Code\Code.exe")
-$ShortcutLocation = [System.IO.Path]::GetFullPath("$env:AppData\Microsoft\Windows\SendTo\VS Code Diff Tool.lnk")
+$ExeInstallationRelativePath = "Microsoft VS Code\Code.exe"
+
+if ((Test-Path -Path "${env:AppData}\..\Local\Programs\$ExeInstallationRelativePath")){
+$ExePath = Resolve-Path -Path "${env:AppData}\..\Local\Programs\$ExeInstallationRelativePath"
+}
+
+if ((Test-Path -Path "${env:ProgramFiles(x86)}\$ExeInstallationRelativePath")){
+$ExePath = Resolve-Path -Path "${env:ProgramFiles(x86)}\$ExeInstallationRelativePath"
+}
+
+if ((Test-Path -Path "${env:ProgramFiles}\$ExeInstallationRelativePath")){
+$ExePath = Resolve-Path -Path "${env:ProgramFiles}\$ExeInstallationRelativePath"
+}
+
+$ExePathString = $ExePath.ToString()
+
+$ShortcutLocation = [System.IO.Path]::GetFullPath("${env:AppData}\Microsoft\Windows\SendTo\VS Code Diff.lnk")
 $WScriptShell = New-Object -ComObject WScript.Shell
 $Shortcut = $WScriptShell.CreateShortcut($ShortcutLocation)
-$Shortcut.TargetPath = $ExePath
+$Shortcut.TargetPath = $ExePathString
 $Shortcut.Arguments = "--new-window --disable-extensions --diff"
 $Shortcut.Save()
-Write-Output "Created compare action $ShortcutLocation"
+Write-Output "Created compare action $ShortcutLocation to $ExePathString"
